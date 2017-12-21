6736dcfae0caa878aec91acaf9be8825b5e14fa8
jackson <jo80923@outlook.com>
2017-12-20 22:22:23 -0500

    interior and events cms done

--- src/main/resources/static/js/gallery.js
+++ src/main/resources/static/js/gallery.js
@@ -3,32 +3,24 @@
  */
 app.controller('galleryCtrl', ['$scope', '$http', //scope = model for angular, http = object used for http transactions
     function ($scope, $http) {
-        $('.lightSlider').lightSlider({
-            gallery: true,
-            item: 1,
-            loop: true,
-            slideMargin: 0,
-            thumbItem: 5,
-        });
-
-        $scope.photoList =null;
+        $scope.photoList = null;
         $scope.groups = null;
         $scope.sections = null;
 
         $scope.loadEditData = function () {
-            var url = "http://"+window.location.hostname+":8080/admin/galleryGroups";
+            var url = "http://" + window.location.hostname + ":8080/admin/galleryGroups";
             $http.get(url).success(
                 function (response) {
                     $scope.groups = response;
                 }
             );
-            url = "http://"+window.location.hostname+":8080/admin/gallerySections";
+            url = "http://" + window.location.hostname + ":8080/admin/gallerySections";
             $http.get(url).success(
                 function (response) {
                     $scope.sections = response;
                 }
             );
-            url = "http://"+window.location.hostname+":8080/admin/photoList";
+            url = "http://" + window.location.hostname + ":8080/admin/photoList";
             $http.get(url).success(
                 function (response) {
                     $scope.photoList = response;
@@ -37,9 +29,9 @@ app.controller('galleryCtrl', ['$scope', '$http', //scope = model for angular, h
         };
 
         $scope.deleteGroup = function (id) {
-            var url = "http://"+window.location.hostname+":8080/admin/deleteGroup?group="+id;
+            var url = "http://" + window.location.hostname + ":8080/admin/deleteGroup?group=" + id;
             var result = confirm("Are you sure? Any changes you have made will go live on the site.");
-            if(result){
+            if (result) {
                 $http.get(url).success(
                     function () {
                         alert("Your changes to gallery page have gone live");
@@ -50,9 +42,9 @@ app.controller('galleryCtrl', ['$scope', '$http', //scope = model for angular, h
         };
 
         $scope.deleteSection = function (id) {
-            var url = "http://"+window.location.hostname+":8080/admin/deleteSection?section="+id;
+            var url = "http://" + window.location.hostname + ":8080/admin/deleteSection?section=" + id;
             var result = confirm("Are you sure? Any changes you have made will go live on the site.");
-            if(result){
+            if (result) {
                 $http.get(url).success(
                     function () {
                         alert("Your changes to gallery page have gone live");
@@ -62,18 +54,18 @@ app.controller('galleryCtrl', ['$scope', '$http', //scope = model for angular, h
             }
         };
 
-        $scope.selectGroupSection1 = function (section ,group) {
+        $scope.selectGroupSection1 = function (section, group) {
             group.section1 = section;
         };
-        $scope.selectGroupSection2 = function (section ,group) {
+        $scope.selectGroupSection2 = function (section, group) {
             group.section2 = section;
         };
-        $scope.selectGroupSection3 = function (section ,group) {
+        $scope.selectGroupSection3 = function (section, group) {
             group.section3 = section;
         };
 
         $scope.newGroup = function () {
-            var url = "http://"+window.location.hostname+":8080/admin/newGroup";
+            var url = "http://" + window.location.hostname + ":8080/admin/newGroup";
             $http.get(url).success(
                 function () {
                     window.location.reload();
@@ -82,164 +74,179 @@ app.controller('galleryCtrl', ['$scope', '$http', //scope = model for angular, h
         };
 
         $scope.saveGroup = function (group) {
-            var url = "http://"+window.location.hostname+":8080/admin/saveGroup";
-            $http.post(url,group).success(
+            var url = "http://" + window.location.hostname + ":8080/admin/saveGroup";
+            $http.post(url, group).success(
                 function () {
                     window.location.reload();
                 }
             );
         };
-        
+
         $scope.removePhoto = function (photo, section) {
             var index = section.photos.indexOf(photo);
-            if(index>-1){
-                section.photos.splice(index,1);
+            if (index > -1) {
+                section.photos.splice(index, 1);
             }
-        }
+        };
 
         $(document).ready(function () {  //this is a monster of a method @jackson
             $(".internal-gallery").hide();
             $(".close-internal").hide();
+            var activeSlider;
             $('.expandable').on('click', function () {
                 if (!$(this).hasClass('active')) {
                     $("h1").fadeOut().hide();
                     $this = $(this);
                     $this.addClass('active');
+                    $currentContainer = $this.parent();
+                    $currentGroup = $currentContainer.parent();
+                    $currentGroup.scrollTop($currentGroup.scrollHeight - $currentGroup[0].clientHeight);
+                    $this.find('.lightSlider').hide();
                     $this.find(".internal-gallery").hide().fadeIn(1000);
                     if ($this.hasClass('left')) {
-                        $("#right-container").fadeOut(1000);
-                        if ($('#left-container > :not(.active)').length) {
-                            $('#left-container > :not(.active)').fadeOut(1000, function () {
-                                $("#left-container").stop().animate({
-                                    width: "100%"
+                        $currentGroup.find(".right-container").fadeOut(1000);
+                        if ($this.hasClass("first-left")) {
+                            $currentContainer.find('.second-left').fadeOut(1000, function () {
+                                $currentContainer.stop().animate({
+                                    width: "98%"
                                 }, 1000, function () {
                                     $this.stop().animate({
                                         height: "100%"
                                     }, 1000, function () {
-                                        $this.find("ul").hide().fadeIn(1000, function () {
-                                            $(window).trigger('resize');
-                                        });
-                                        $this.find(".close-internal").hide().fadeIn(function () {
-                                            $(window).trigger('resize');
-                                        });
-
+                                        $this.find("ul").hide().show();
+                                        $this.find(".close-internal").hide().show();
+                                        $(window).trigger('resize');
+                                        if (!$this.find(".lightSlider").hasClass("created")) {
+                                            $this.find('.lightSlider').lightSlider({
+                                                gallery: true,
+                                                item: 1,
+                                                loop: true,
+                                                slideMargin: 0,
+                                                thumbItem: 25
+                                            });
+                                            $this.find(".lightSlider").addClass("created");
+                                        }
+                                        $(window).trigger('resize');
                                     });
                                 });
                             });
                         }
-                        else{
-                            $('#left-container > :not(.active)').fadeOut(1000, function () {
-                                $("#left-container").stop().animate({
-                                    width: "100%"
+                        else {
+                            $currentContainer.find('.first-left').animate({
+                                height: "0"
+                            }, 1000, function () {
+                                $this.stop().animate({
+                                    height: "100%"
                                 }, 1000, function () {
-                                    $this.stop().animate({
-                                        height: "100%"
+                                    $currentContainer.stop().animate({
+                                        width: "98%"
                                     }, 1000, function () {
-                                        $this.find("ul").hide().fadeIn(1000);
-                                        $this.find(".close-internal").hide().fadeIn(function () {
-                                            $(window).trigger('resize');
-                                        });
+                                        $this.find("ul").hide().show();
+                                        $this.find(".close-internal").hide().show();
+                                        $(window).trigger('resize');
+                                        if (!$this.find(".lightSlider").hasClass("created")) {
+                                            $this.find('.lightSlider').lightSlider({
+                                                gallery: true,
+                                                item: 1,
+                                                loop: true,
+                                                slideMargin: 0,
+                                                thumbItem: 25
+                                            });
+                                            $this.find(".lightSlider").addClass("created");
+                                        }
+                                        $(window).trigger('resize');
                                     });
                                 });
                             });
                         }
                     }
-                    else{
-                        $("#left-container").fadeOut(1000, function () {
-                            $("#right-container").css({"margin-left": "61.5%"});
-                            if ($('#right-container > :not(.active)').length) {
-                                $('#right-container > :not(.active)').fadeOut(1000, function () {
-                                    $("#right-container").stop().animate({
-                                        width: "100%",
-                                        marginLeft: "0"
-                                    }, 1000, function () {
-                                        $this.stop().animate({
-                                            height: "100%"
-                                        }, 1000, function () {
-                                            $this.find("ul").hide().fadeIn(1000);
-                                            $this.find(".close-internal").hide().fadeIn(1000,function () {
-                                                $(window).trigger('resize');
-                                            });
-                                        });
-                                    });
-
-                                });
-                            }
-                            else{
-                                $("#right-container").stop().animate({
-                                    width: "100%",
-                                    marginLeft: "0"
+                    else {
+                        $currentGroup.find(".left-container").fadeOut(1000, function () {
+                            $currentContainer.css({"margin-left": "61.5%"});
+                            $currentContainer.stop().animate({
+                                width: "99%",
+                                marginLeft: "0"
+                            }, 1000, function () {
+                                $this.stop().animate({
+                                    height: "100%"
                                 }, 1000, function () {
-                                    $this.stop().animate({
-                                        height: "100%"
-                                    }, 1000, function () {
-                                        $this.find("ul").hide().fadeIn(1000);
-                                        $this.find(".close-internal").hide().fadeIn(1000,function () {
-                                            $(window).trigger('resize');
+                                    $this.find("ul").hide().show();
+                                    $this.find(".close-internal").hide().show();
+                                    $(window).trigger('resize');
+                                    if (!$this.find(".lightSlider").hasClass("created")) {
+                                        $this.find('.lightSlider').lightSlider({
+                                            gallery: true,
+                                            item: 1,
+                                            loop: true,
+                                            slideMargin: 0,
+                                            thumbItem: 25
                                         });
-
-                                    });
+                                        $this.find(".lightSlider").addClass("created");
+                                    }
+                                    $(window).trigger('resize');
                                 });
-
-                            }
+                            });
                         });
-
                     }
                 }
-
             });
-
             $(".close-internal").on('click', function () {
+                $this = $(this).parent().parent();
+                $currentContainer = $this.parent();
+                $currentGroup = $currentContainer.parent();
                 $this.find(".close-internal").fadeOut();
                 $this.find("ul").hide();
-                if ($(".active").hasClass("right")) {
-                    $(".expandable.active").stop().animate({
-                        height: '92%'
+                if ($this.hasClass("right")) {
+                    $this.stop().animate({
+                        height: '94%'
                     }, 1000, function () {
-                        $("#right-container").stop().animate({
+                        $currentContainer.stop().animate({
                             width: '38.5%',
                             marginLeft: '58.5%'
                         }, 1000, function () {
-                            $("#right-container").css({marginLeft: '0'});
-                            if ($('#right-container > :not(.active)').length) {
-                                $('#right-container > :not(.active)').hide().fadeIn();
-                                $("#left-container").hide().fadeIn();
-                            }
-                            else{
-                                $("#left-container").hide().fadeIn();
-
-                            }
+                            $currentContainer.css({marginLeft: '0'});
+                            $this.find(".internal-gallery").fadeOut();
+                            $currentGroup.find(".left-container").hide().fadeIn();
+                            $this.find(".close-internal").fadeOut();
                             $("h1").hide().fadeIn();
-                            $(".expandable").removeClass('active');
-                            $(".internal-gallery").fadeOut();
-                            $(".close-internal").fadeOut();
-
+                            $this.removeClass('active');
                         });
                     });
                 }
                 else {
-                    $(".expandable.active").stop().animate({
-                        height: '46%'
-                    }, 1000, function () {
-                        $("#left-container").stop().animate({
-                            width: '58.5%',
+                    if ($this.hasClass("first-left")) {
+                        $this.stop().animate({
+                            height: '46%'
                         }, 1000, function () {
-                            if ($('#left-container > :not(.active)').length) {
-                                $('#left-container > :not(.active)').hide().fadeIn();
-                                $("#right-container").hide().fadeIn();
-
-                            }
-                            else{
-                                $("#right-container").hide().fadeIn();
-                            }
-
-                            $("h1").hide().fadeIn();
-                            $(".expandable").removeClass('active');
-                            $(".internal-gallery").fadeOut();
-                            $(".close-internal").fadeOut();
-
+                            $currentContainer.stop().animate({
+                                width: '58.5%',
+                            }, 1000, function () {
+                                $currentContainer.find('.second-left').hide().fadeIn();
+                                $currentGroup.find(".right-container").hide().fadeIn();
+                                $("h1").hide().fadeIn();
+                                $this.removeClass('active');
+                                $this.find(".internal-gallery").fadeOut();
+                                $this.find(".close-internal").fadeOut();
+                            });
                         });
-                    });
+                    }
+                    else {
+                        $currentContainer.stop().animate({
+                            width: '58.5%'
+                        }, 1000, function () {
+                            $currentContainer.find(".first-left").css({"opacity": "0"}).animate({height: "46%"}, 1000).animate({opacity: "1"});
+                            $this.stop().animate({
+                                height: '46%'
+                            }, 1000, function () {
+                                $currentGroup.find(".right-container").hide().fadeIn();
+                                $("h1").hide().fadeIn();
+                                $this.removeClass('active');
+                                $this.find(".internal-gallery").fadeOut();
+                                $this.find(".close-internal").fadeOut();
+
+                            });
+                        });
+                    }
                 }
             });
         });
